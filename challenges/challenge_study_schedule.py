14de1e43cf7a007e21303d0ff278e1220e50ed0c
Diene89 <diene_89@hotmail.com>
2022-11-21 15:40:38 -0300

    feat: implements study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+    students = 0
+
+    for start_time, end_time in permanence_period:
+        if not isinstance(start_time, int) or not isinstance(end_time, int):
+            return None
+        elif start_time <= target_time <= end_time:
+            students += 1
+    return students
