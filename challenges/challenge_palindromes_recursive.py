e286281791cfda2cb4ec254e8cfcb8ecfbec1d49
Diene89 <diene_89@hotmail.com>
2022-11-21 17:27:29 -0300

    refactor: function challenge_palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,5 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if not word == '':
+    if not word or word == '':
         return False
     if low_index >= high_index:
         return True
